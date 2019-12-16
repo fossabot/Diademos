4bcc3d31440ce11aeb16f76b366826011479b2fd
Emil Sayahi <em@emsa.cf>
2019-12-15 21:45:20 -0500

    Initial commit

---
+++ Data/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diademos.Data
+{
+    public class Article
+    {
+        public DateTime DatePublished { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Author { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Contents { get; set; }
+
+        public string URL { get; set; }
+
+        public string Tags { get; set; }
+    }
+}
---
+++ Data/News.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// TODO: Get Unirest for C#
+
+namespace Diademos.Data
+{
+    public class News
+    {
+        public Task<Article[]> GetForecastAsync(DateTime startDate, String orderby, int feedInd)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(feedInd, feedInd + 10).Select(index => new Article
+            {
+                Date = ,
+                Publisher = ,
+                Author = ,
+                Summary = ,
+                Contents = ,
+                URL = ,
+                Tags = ,
+            }).ToArray());
+        }
+    }
+}
+
+/*
+ * Hoaxy:
+Task<HttpResponse<MyClass>> response = Unirest.get("https://api-hoaxy.p.rapidapi.com/articles?sort_by=relevant&use_lucene_syntax=true&query=canonical_url%253A+%2522" + URL +"%2522")
+.header("X-RapidAPI-Host", "api-hoaxy.p.rapidapi.com")
+.header("X-RapidAPI-Key", "c594e3fb9emsh95f74bae064e059p1e1959jsnc4df23dcc4ef")
+.asJson();
+*/
